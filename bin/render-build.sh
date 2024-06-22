be3a271854073840050f89999ead8794093e38a7
Art <honeydewart@gmail.com>
2024-06-22 21:51:28 +0800

    added db:seed to render build

--- bin/render-build.sh
+++ bin/render-build.sh
@@ -7,5 +7,7 @@ chmod +x ./bin/rails
 bundle install
 bundle exec rails assets:precompile
 bundle exec rails assets:clean
+bundle exec rails db:drop
+bundle exec rails db:create
 bundle exec rails db:migrate
 bundle exec rails db:seed
